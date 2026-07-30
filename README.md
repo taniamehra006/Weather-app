2e4f2d2870b63c25a0b80d5b433a174bc6167b2e
Tania <taniamehra2006@gmail.com>
2026-07-30 13:09:07 +0530

    Weather App

---
+++ README.md
@@ -0,0 +1,35 @@
+# Weather App 🌦️
+
+A simple Python weather application that fetches live weather data using the OpenWeather API.
+
+## Features
+
+- Search weather by city
+- Temperature in Celsius
+- Feels Like temperature
+- Humidity
+- Weather description
+- Wind speed
+- Invalid city handling
+
+## Technologies Used
+
+- Python
+- Requests
+- OpenWeather API
+
+## Installation
+
+```bash
+pip install requests
+```
+
+## Run
+
+```bash
+python main.py
+```
+
+## Author
+
+Tania Mehra
---
+++ main.py
@@ -0,0 +1,31 @@
+import requests 
+
+API_KEY = "bc4c52e007364600279abc1b6b40af9f"
+
+city = input("Enter you city name:-")
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+
+data = response.json()
+
+if data["cod"] != 200:
+    print("❌ City not found!")
+else:
+    city = data["name"]
+    temperature = data["main"]["temp"]
+    feels_like = data["main"]["feels_like"]
+    humidity = data["main"]["humidity"]
+    weather = data["weather"][0]["main"]
+    description = data["weather"][0]["description"]
+    wind_speed = data["wind"]["speed"]
+
+    print("\n------ Weather Report ------")
+    print("City        :", city)
+    print("Temperature :", temperature, "°C")
+    print("Feels Like  :", feels_like, "°C")
+    print("Humidity    :", humidity, "%")
+    print("Weather     :", weather)
+    print("Description :", description)
+    print("Wind Speed  :", wind_speed, "m/s")
